de84a9d3c3533ac1f8ef705d4c0601b35329df5e
Selva Prasath Selvamani <selvaprasath.selvamani@bounteous.com>
2023-07-11 17:46:56 +0530

    Added a config fix

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 const nextConfig = {
   env: {
     clientId: process.env.clientId,
-     authority: process.env.authority,
-      clientSecret: process.env.clientSecret,
-       redirectUri: process.env.redirectUri,
+    authority: process.env.authority,
+    clientSecret: process.env.clientSecret,
+    redirectUri: process.env.redirectUri,
   },
+  appDir: true,
 };
 
 module.exports = nextConfig
