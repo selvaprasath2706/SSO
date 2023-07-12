e1aa468b07ec5382b60084a56206ba34e7142697
Selva Prasath Selvamani <selvaprasath.selvamani@bounteous.com>
2023-07-12 22:31:56 +0530

    Fix for build failed

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,10 +1,14 @@
-"use client"
+"use client";
 
 export default function Authentication() {
-     function getCookie(n:string) {
-       let a = `; ${document.cookie}`.match(`;\\s*${n}=([^;]+)`);
-       return a ? a[1] : "";
-     }
+  function getCookie(n: string) {
+    if (window.document) {
+      let a = `; ${window.document.cookie}`.match(`;\\s*${n}=([^;]+)`);
+      return a ? a[1] : "";
+    } else {
+      return "0";
+    }
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
